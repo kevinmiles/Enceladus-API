b81b6bef6f624ad25193cc1e27119f8cdb277442
Jacob Pratt <the.z.cuber@gmail.com>
2019-01-26 00:55:05 -0500

    Add Linguist setting

--- build.rs
+++ build.rs
@@ -1,13 +1,9 @@
 use diesel::prelude::*;
 use diesel_migrations::run_pending_migrations;
 use dotenv::dotenv;
-use std::{env, fs::create_dir};
+use std::env;
 
 fn run_migrations() {
-    // If this fails, either the directory exists or the user lacks permission.
-    // In the latter case, the user will get an error when trying to establish a connection.
-    let _ = create_dir("databases");
-
     let db_url = env::var("DATABASE_URL").expect("environment variable DATABASE_URL must be set");
 
     run_pending_migrations(
